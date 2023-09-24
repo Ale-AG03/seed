ba5bf028f966be28df6f08d3231c51f9b173ce75
Ale-AG03 <Albertodeorozcof03@icloud.com>
2023-09-24 17:21:15 -0600

    1

---
+++ php/index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuestionario de abuso de Drogas</title>
+</head>
+<body>
+    <form action="">
+        <section>
+            <label for="">Nombre de la PPL solicitante o usuario del programa:</label>
+            <label for="">No. Expediente:<label>
+            <input type="text" name="" id="">
+            <input type="text" name="" id="">
+            <input type="text" name="" id="">
+            <input type="date" name="" id="">
+            <input type="number" name="" id="">
+            
+            <input type="time" name="" id="">
+        </section>
+    
+    
+
+    </form>
+    
+
+    
+</body>
+</html>
