a4126c01652e61cdd2c1da99f062a8a98fcfb8e0
Ale-AG03 <Albertodeorozcof03@icloud.com>
2023-09-24 18:17:15 -0600

    2

--- php/index.php
+++ php/index.php
@@ -8,27 +8,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/cues_abuso_drogas.css">
+
+
     <title>Cuestionario de abuso de Drogas</title>
 </head>
 <body>
+    <div class="container">   
+        <div>img</div>
+        <div>img</div>
+    </div>
     <form action="">
+        <div class="container">
+
         <section>
-            <label for="">Nombre de la PPL solicitante o usuario del programa:</label>
-            <label for="">No. Expediente:<label>
-            <input type="text" name="" id="">
-            <input type="text" name="" id="">
-            <input type="text" name="" id="">
-            <input type="date" name="" id="">
-            <input type="number" name="" id="">
-            
-            <input type="time" name="" id="">
+            <article>
+                <label for="" class="">Nombre de la PPL solicitante o usuario del programa:</label>
+                <label for="" class="">No. Expediente: 134134213<label>
+            </article>  
         </section>
-    
-    
+
+
+
+        <section>
+            
+            <input type="text" name="" id="" placeholder="Nombre">
+            <input type="text" name="" id="" placeholder="Apellido Materno">
+            <input type="text" name="" id="" placeholder="Apellido Paterno">
+            <input type="date" name="" id="">
+            <input type="number" name="" id="" placeholder="Edad">
+            <select class="form-select" aria-label="Default select example">
+            <option selected>Seleciona uno</option>
+                <option value="1">Hombre</option>
+                <option value="2">Mujer</option>
+            </select>
+            <input type="time" name="" id="">
+            <input type="text" name="" id="" placeholder="Ubicacion">
+
+        </section>
+
+        </div>
+        
+
 
     </form>
     
 
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
